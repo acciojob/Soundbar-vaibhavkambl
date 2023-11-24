d9d36eac603f6cb1ba2041bffbbef8c42cbcba40
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-24 12:30:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function playSound(soundFileName) {
+      const audio = new Audio(`sounds/${soundFileName}`);
+      audio.play();
+    }
+
+    function stopSounds() {
+      const audioElements = document.querySelectorAll('audio');
+      audioElements.forEach(audio => audio.pause());
+    }
