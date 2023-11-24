68bdd61d83593f7f327a97a2664ec67c22823e77
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-24 12:41:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 //your JS code here. If required.
-function playSound(soundFileName) {
-      const audio = new Audio(`sounds/${soundFileName}`);
-      audio.play();
-    }
+function expectPlayingAudio() {
+  cy.get('audio,video').should('have.prop', 'paused', false);
+}
 
-    function stopSounds() {
-      const audioElements = document.querySelectorAll('audio');
-      audioElements.forEach(audio => audio.pause());
-    }
+// Add this helper function to wait for audio to be ready
+function waitForAudioToBeReady() {
+  return new Promise((resolve) => {
+    cy.get('audio,video').on('canplay', () => {
+      resolve();
+    });
+  });
+}
+
+// Modify your test cases
+it("Test sound 1", () => {
+  cy.visit(baseUrl + "/main.html");
+  cy.get(".btn").first().click();
+  // Wait for audio to be ready
+  waitForAudioToBeReady().then(() => {
+    expectPlayingAudio();
+  });
+});
+
+it("Test sound 2", () => {
+  cy.visit(baseUrl + "/main.html");
+  cy.get(".btn").eq(1).click();
+  // Wait for audio to be ready
+  waitForAudioToBeReady().then(() => {
+    expectPlayingAudio();
+  });
+});
+
+// Repeat for other test cases...
